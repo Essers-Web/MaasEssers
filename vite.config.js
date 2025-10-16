5c7663f07f0bbc44976b4c42c779763211ada3dc
Maassrs <m.essers@sintlucasedu.nl>
2025-10-17 00:45:37 +0200

    pushing portfolio dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base:'/MaasEssers/',
+    base:'./',
     homepage: "https://essers-web.github.io/MaasEssers/",
     plugins: [vue()],
 })
